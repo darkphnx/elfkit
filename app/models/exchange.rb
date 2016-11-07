c9c0351171b4076f6dc89da92f165dc302618c67
Dan Wentworth <dan@atechmedia.com>
2016-11-07 19:15:32 +0000

    match up participants

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -6,4 +6,14 @@ class Exchange < ApplicationRecord
 
   validates :title, presence: true
   validates :permalink, presence: true
+
+  def pair_participants!
+    matchmaker.pair.each do |gifter, giftee|
+      participant_matches.create!(gifter: gifter, giftee: giftee)
+    end
+  end
+
+  def matchmaker
+    @matchmaker ||= Elfkit::Matchmaker.new(participants)
+  end
 end
