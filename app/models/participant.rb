453c0154042cc4cb79791a36432a563a955b028c
Dan Wentworth <dan@atechmedia.com>
2016-11-08 11:59:03 +0000

    ensure participants have a login token

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,8 +1,13 @@
 class Participant < ApplicationRecord
   include RandomPermalink
-  
+
   belongs_to :exchange
 
   validates :name, presence: true
   validates :email_address, presence: true
+  validates :login_token, presence: true
+
+  before_validation do
+    self.login_token ||= SecureRandom.urlsafe_base64
+  end
 end
