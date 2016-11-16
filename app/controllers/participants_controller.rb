b6055fb051e471f5753eee78ca0b87007779ee21
Dan Wentworth <dan@atechmedia.com>
2016-11-16 15:28:54 +0000

    join/leave buttons

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,10 +1,17 @@
 class ParticipantsController < ApplicationController
   before_action do
     @exchange = Exchange.find_by_permalink(params[:exchange_id])
+    @participant = @exchange.participants.find_by_permalink(params[:id])
+  end
+
+  before_action except: :create do
+    if @participant != current_participant
+      redirect_to exchange_path(@exchange_path), "You need to be logged in to do that"
+    end
   end
 
   def create
-    @participant = @exchange.participants.build(safe_participant_params)
+    @participant = @exchange.participants.build(safe_create_participant_params)
 
     if @participant.save
       redirect_to exchange_path(@exchange), notice: "Invite sent, please check your e-mail"
@@ -13,9 +20,21 @@ class ParticipantsController < ApplicationController
     end
   end
 
+  def update
+    if @participant.update(safe_update_participant_params)
+      redirect_to exchange_path(@exchange)
+    else
+      redirect_to exchange_path(@exchange), alert: "Sorry, couldn't update your preferences at this time"
+    end
+  end
+
   private
 
-  def safe_participant_params
+  def safe_create_participant_params
     params.require(:participant).permit(:name, :email_address)
+  end
+
+  def safe_update_participant_params
+    params.require(:participant).permit(:participating)
   end
 end
