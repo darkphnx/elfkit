e3d98f4f5d84f7583fc21e9854eecc331991e7fc
Dan Wentworth <dan@atechmedia.com>
2016-11-17 16:51:23 +0000

    reminder before matching

--- app/mailers/participant_mailer.rb
+++ app/mailers/participant_mailer.rb
@@ -6,6 +6,13 @@ class ParticipantMailer < ApplicationMailer
     mail to: @participant.email_tag, subject: "#{@exchange.title} - Confirm your participation"
   end
 
+  def match_reminder(participant)
+    @participant = participant
+    @exchange = participant.exchange
+
+    mail to: @participant.email_tag, subject: "#{@exchange.title} - It's nearly matching time!"
+  end
+
   def match_ready(participant)
     @participant = participant
     @exchange = participant.exchange
