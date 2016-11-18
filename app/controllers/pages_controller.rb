89de19107ca5af3df54b680e71e7a7d9b3c0f8f5
Dan Wentworth <dan@atechmedia.com>
2016-11-18 15:41:07 +0000

    Homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,4 @@
 class PagesController < ApplicationController
-  def index
-    render text: "hello"
+  def home
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'logout', to: 'sessions#destroy', as: :logout
+  root to: 'pages#home'
 
-  resources :exchanges, path: '/' do
-    resources :participants do
+  resources :exchanges, except: [:index, :destroy], path: '/' do
+    resources :participants, only: [:create, :update] do
       get 'retrieve/:login_token', to: 'sessions#create', as: :retrieve
     end
   end
