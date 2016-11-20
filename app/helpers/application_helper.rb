771d14f1c8939eaad745d9935da5fa08f559ea0a
Dan Wentworth <dan@atechmedia.com>
2016-11-20 18:10:21 +0000

    facebook and twitter metatags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
+  DEFAULT_META_TAGS = {
+    'twitter:card' => 'summary',
+    'twitter:site' => '@darkphnx',
+    'twitter:creator' => '@darkphnx',
+    'og:title' => "Elfk.it",
+    'og:description' => "Gift Exchange Made Easy - Manage your next gift exchange with Elfkit.",
+    'og:url' => "https://elfk.it",
+    'og:image' => 'https://elfk.it/og-image.png'
+  }.freeze
+
   def title(set = nil)
     @title = set if set
 
@@ -6,6 +16,24 @@ module ApplicationHelper
       s << @title << ' - ' if @title
       s << 'Elfkit'
     end
+  end
+
+  def meta(name = nil, value = nil)
+    @meta ||= DEFAULT_META_TAGS.dup
+    @meta[name] = value if name
+    @meta
+  end
+
+  def meta_tags
+    tags = meta.map do |(name, value)|
+      if name.start_with?('og:')
+        tag(:meta, property: name, content: value)
+      else
+        tag(:meta, name: name, content: value)
+      end
+    end
+
+    safe_join(tags, "\n")
   end
 
   def display_flash
